cfcf867bc182e7e0f832f7530e0463ca3a34d6a5
Noah Allsbrook <scimatwri@gmail.com>
2022-03-02 10:29:50 -0500

    SAVE AND OPEN WORK NOW

--- main.py
+++ main.py
@@ -32,17 +32,33 @@ class Window(QWidget):
 
                 with f:
                     f.write(data)
-                    
+
             except FileNotFoundError:
                     print("File not found.")
+
+        def open_file():
+
+            filename = QFileDialog.getOpenFileName(self, "Save File", path, "(*.txt)")
+            f = open(filename[0], "r")
+
+            with f:
+                data = f.read()
+                textarea.setText(data)
 
         toolbar = QToolBar()
         savebutton = QToolButton()
         savebutton.setText("Save")
         savebutton.setStyleSheet("background-color: #434343; font-size: 14px; font-family: Open Sans; color: #FFFFFF")
         savebutton.clicked.connect(save)
+
+        openbutton = QToolButton()
+        openbutton.setText("Open")
+        openbutton.setStyleSheet("background-color: #434343; font-size: 14px; font-family: Open Sans; color: #FFFFFF")
+        openbutton.clicked.connect(open_file)
+
         layout.addWidget(toolbar)
-        layout.addWidget(savebutton)
+        toolbar.addWidget(savebutton)
+        toolbar.addWidget(openbutton)
 
         textarea = QTextEdit(self)
         textarea.setPlaceholderText("Enter text...")
