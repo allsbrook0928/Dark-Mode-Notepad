e30136baec27265aa1ae872e2a352a0f8256ef23
allsbrook0928 <scimatwri@gmail.com>
2022-03-07 10:03:37 -0500

    Changed main font from Fira Mono to Ubuntu Mono

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ class Window(QWidget):
         super(Window, self).__init__()
         self.setWindowTitle("Dark Mode Notepad")
 
-        self.setStyleSheet("background-color: #1A303A; font-size: 18px; font-family: Fira Mono, Open Sans")
+        self.setStyleSheet("background-color: #1A303A; font-size: 18px; font-family: Ubuntu Mono, Open Sans")
 
         layout = QGridLayout()
         self.setLayout(layout)
 
         self.resize(853, 480)
-        
+
         def save():
 
             filename = QFileDialog.getSaveFileName(self, "Save File", path, "*.txt")
@@ -72,18 +72,18 @@ class Window(QWidget):
             colors = [red, orange, yellow, green, blue, purple, pink]
             color = random.choice(colors)
 
-            self.setStyleSheet(f"{color}; font-size: 18px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
-            savebutton.setStyleSheet(f"{color}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
-            openbutton.setStyleSheet(f"{color}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+            self.setStyleSheet(f"{color}; font-size: 18px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
+            savebutton.setStyleSheet(f"{color}; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
+            openbutton.setStyleSheet(f"{color}; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
             #colorchange.setStyleSheet(f"{color}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
             #resetbgbutton.setStyleSheet(f"{color}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
-        
+
         def reset_bg():
 
             reset = "background-color: #232323"
-            self.setStyleSheet(f"{reset}; font-size: 18px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
-            savebutton.setStyleSheet(f"{reset}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
-            openbutton.setStyleSheet(f"{reset}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+            self.setStyleSheet(f"{reset}; font-size: 18px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
+            savebutton.setStyleSheet(f"{reset}; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
+            openbutton.setStyleSheet(f"{reset}; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
             #colorchange.setStyleSheet(f"{reset}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
             #resetbgbutton.setStyleSheet(f"{reset}; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
 
@@ -91,45 +91,45 @@ class Window(QWidget):
 
             text = textarea.toPlainText()
             count = 0
-            
+
             for word in text.split(" "):
-                
+
                 word = text.strip()
                 if word:
                     count += 1
 
             wordcount_label.setText(f"Total Words: {count}")
 
-            
+
 
         toolbar = QToolBar()
         savebutton = QToolButton()
         savebutton.setText("Save")
-        savebutton.setStyleSheet("background-color: #13232A; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+        savebutton.setStyleSheet("background-color: #13232A; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
         savebutton.clicked.connect(save)
 
         openbutton = QToolButton()
         openbutton.setText("Open")
-        openbutton.setStyleSheet("background-color: #13232A; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+        openbutton.setStyleSheet("background-color: #13232A; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
         openbutton.clicked.connect(open_file)
 
         wordcount_button = QToolButton()
         wordcount_button.setText("Get Word Count")
-        wordcount_button.setStyleSheet("background-color: #13232A; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+        wordcount_button.setStyleSheet("background-color: #13232A; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
         wordcount_button.clicked.connect(get_word_count)
 
         wordcount_label = QLabel()
         wordcount_label.setText("")
-        wordcount_label.setStyleSheet("font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+        wordcount_label.setStyleSheet("font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
 
         """colorchange = QToolButton()
         colorchange.setText("Change Background Color")
-        colorchange.setStyleSheet("background-color: #232323; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+        colorchange.setStyleSheet("background-color: #232323; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
         colorchange.clicked.connect(change_bg)"""
 
         """resetbgbutton = QToolButton()
         resetbgbutton.setText("Reset the Background Color")
-        resetbgbutton.setStyleSheet("background-color: #232323; font-size: 14px; font-family: Fira Mono, Open Sans; color: #FFFFFF")
+        resetbgbutton.setStyleSheet("background-color: #232323; font-size: 14px; font-family: Ubuntu Mono, Open Sans; color: #FFFFFF")
         resetbgbutton.clicked.connect(reset_bg)"""
 
         layout.addWidget(toolbar)
